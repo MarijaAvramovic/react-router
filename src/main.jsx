3fc5366bcfede0b2b3cf71b122d0cc333d8eb19b
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-03-19 20:11:40 -0400

    default component

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Profile from './Profile.jsx';
 import App from './App.jsx'
 import Spinach from './Spinach.jsx';
 import Popeye from './Popeye.jsx';
+import DefaultProfile from './DefaultProfile.jsx';
 
 const router = createBrowserRouter([
   {path: "/",
@@ -15,6 +16,7 @@ const router = createBrowserRouter([
     path: "profile",
     element: <Profile />,
     children: [
+      {index: true, element: <DefaultProfile />},
       {path: "spinach", element: <Spinach />},
       {path: "popeye", element: <Popeye />}
     ],
